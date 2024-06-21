a6bc264190732826c555150976a0411763a3ac6c
pmpwsk <flo@uwap.org>
2024-06-21 18:45:00 +0200

    Changed the handler for WF 3.0.

---
+++ Handler.cs
@@ -0,0 +1,16 @@
+﻿namespace uwap.WebFramework.Plugins;
+
+public partial class RedirectPlugin : Plugin
+{
+    public override Task Handle(Request req)
+    {
+        if (req.Method != "GET")
+            throw new BadMethodSignal();
+
+        if (Redirects.TryGetValue(req.Path, out string? value))
+            req.Redirect(value);
+        else throw new NotFoundSignal();
+
+        return Task.CompletedTask;
+    }
+}
